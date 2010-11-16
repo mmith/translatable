acf78e6761ac04260d3bbdf0c35cf8a0cd92da2f
Mickael Mithouard <info@mickael-mithouard.com>
2010-11-16 16:02:42 +0100

    initial commit

---
+++ lib/acts_as_translatable.rb
@@ -0,0 +1,29 @@
+module Translatable
+  
+  def self.included(base)
+    base.send :extend, ClassMethods
+  end
+  
+  module ClassMethods
+    def acts_as_translatable
+      accepts_nested_attributes_for :translations
+      after_save :init_translations
+      
+      send :include, InstanceMethods
+    end
+  end
+  
+  module InstanceMethods
+    def init_translations
+      I18n.available_locales.reject{|key| key == :root }.each do |locale|
+        translation = self.translations.find_by_locale locale.to_s
+        if translation.nil?          
+          translations.build :locale => locale
+          save
+        end
+      end
+    end
+  end
+end
+
+ActiveRecord::Base.send :include, Translatable
